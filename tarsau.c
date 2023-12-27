0e2e970d914872ed95c220493065c966d4a69d36
İsa ŞENGÜL <117285107+SengulIsa@users.noreply.github.com>
2023-12-28 02:03:03 +0300

    Add files via upload

--- tarsau.c
+++ tarsau.c
@@ -80,6 +80,93 @@ void tarsau_b(int argc, char *argv[],char *outputf){
 
 }
 
+void tarsau_a(char *archive_file, char *output_directory) {
+    FILE *in_fp = fopen(archive_file, "r");
+    if (in_fp == NULL) {
+        printf("Archive file is inappropriate or corrupt!\n");
+        return;
+    }
+    fclose(in_fp);
+
+    mkdir(output_directory, 0777);
+
+    FILE *archive_fp = fopen(archive_file, "r");
+    if (archive_fp == NULL) {
+        printf("Error opening archive file.\n");
+        return;
+    }
+    int toplam=0;
+    
+    char created_files[32][1000];
+    int total_files=0;
+
+    char file_list[200];
+    fread(file_list, 1, sizeof(file_list), archive_fp);
+    fclose(archive_fp);
+
+    char *file_data = strchr(file_list, '|');
+    while (file_data != NULL) {
+        file_data++; 
+
+        char *file_info_end = strchr(file_data, '|');
+        if (file_info_end != NULL) {
+            *file_info_end = '\0'; 
+
+            char *file_name = strtok(file_data, ",");
+            char *file_perm = strtok(NULL, ",");
+            char *file_content = strtok(NULL, "|");
+
+            if (file_name != NULL && file_perm != NULL && file_content != NULL) {
+                char output_path[256];
+                snprintf(output_path, sizeof(output_path), "%s/%s", output_directory, file_name);
+
+                FILE *out_fp = fopen(output_path, "w");
+                if (out_fp == NULL) {
+                    printf("Error creating file: %s\n", file_name);
+                    return;
+                }
+                int size = atoi(file_content);
+
+                FILE *in_fp1 = fopen(archive_file, "r");
+                char file_list1[200];
+                fread(file_list1, 1, sizeof(file_list1), in_fp1); 
+                int i=toplam;
+                if(i == 0){
+                    while(file_list1[i] != '\n'){
+                        i++;
+                    }
+                    i++;
+                    toplam=i;
+                }
+
+                for(;i<size+toplam;i++){
+                    fprintf(out_fp,"%c",file_list1[i]);
+                }
+                toplam += size;
+                fclose(in_fp1);
+
+                *file_info_end = '|'; 
+                file_data = strchr(file_info_end + 1, '|'); 
+                strcpy(created_files[total_files],file_name);
+                total_files++;
+                
+            } 
+            else {
+                printf("Invalid file format: %s\n", file_data);
+            }
+        } else {
+            printf("Invalid file format: '|' separator not found.\n");
+            break;
+        }
+    }
+
+    for(int i=0;i<total_files;++i){
+        printf("%s ",created_files[i]);
+    }
+
+    printf("Files opened in the directory: %s\n", output_directory);
+}
+
 int main(int argc, char *argv[]) {
 if((argc<3 && (strcmp(argv[1],"-b") || strcmp(argv[1],"-a"))) != 0){
 printf("You must use tarsau -b input files or tarsau -a input files\n");
