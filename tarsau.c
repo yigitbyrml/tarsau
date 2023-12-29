931c5af13f4f3ef58d158e6a4113c4e8e8091852
Yiğit Bayramlı <96197982+yigitbyrml@users.noreply.github.com>
2023-12-29 15:45:11 +0300

    Update tarsau.c

--- tarsau.c
+++ tarsau.c
@@ -4,8 +4,10 @@
 #include <sys/stat.h>
 
 void tarsau_b(int argc, char *argv[],char *outputf){
+
     FILE *outputfs;
     outputfs = fopen(outputf,"w");
+
 
     for(int i=2;i<argc;i++){
         if(strcmp(argv[i],"-o") == 0){
@@ -20,10 +22,10 @@ void tarsau_b(int argc, char *argv[],char *outputf){
         int file_size = ftell(inputfs);
         fseek(inputfs,0,SEEK_SET);
 
-        char record[256];
+        char record[200];
         sprintf(record, "|%s,%o,%d|", argv[i], 0644, file_size); 
-
         fwrite(record, 1, strlen(record), outputfs);
+
 
         if(i==argc-1){
             fprintf(outputfs,"\n");
@@ -33,7 +35,6 @@ void tarsau_b(int argc, char *argv[],char *outputf){
     }
 
     int total_size = 0;
-    
 
     for(int i=2;i<argc;i++){
 
@@ -64,6 +65,7 @@ void tarsau_b(int argc, char *argv[],char *outputf){
         fseek(inputfs,0,SEEK_END);
         int file_size = ftell(inputfs);
         fseek(inputfs,0,SEEK_SET);
+
     
         if(total_size + file_size > 200){
             printf("Total size of input files exceeds 200MB \n");
@@ -76,11 +78,13 @@ void tarsau_b(int argc, char *argv[],char *outputf){
         fread(temp,1,file_size,inputfs);
         fwrite(temp,1,file_size,outputfs);
 
+
         total_size += file_size;
 
         fclose(inputfs);
 
     }
+
     
     fclose(outputfs);
     printf("The files have been merged.\n");
@@ -88,11 +92,13 @@ void tarsau_b(int argc, char *argv[],char *outputf){
 }
 
 void tarsau_a(char *archive_file, char *output_directory) {
+
     FILE *in_fp = fopen(archive_file, "r");
     if (in_fp == NULL) {
         printf("Archive file is inappropriate or corrupt!\n");
         return;
     }
+
 
     int filelen = strlen(archive_file);
     if(filelen < 4 || strcmp(archive_file + filelen - 4, ".sau") != 0){
@@ -101,27 +107,37 @@ void tarsau_a(char *archive_file, char *output_directory) {
     }
     fclose(in_fp);
 
+
     mkdir(output_directory, 0777);
+
 
     FILE *archive_fp = fopen(archive_file, "r");
     if (archive_fp == NULL) {
         printf("Error opening archive file.\n");
         return;
     }
+
     int toplam=0;
     
     char created_files[32][1000];
     int total_files=0;
 
+
     char file_list[200];
     fread(file_list, 1, sizeof(file_list), archive_fp);
     fclose(archive_fp);
 
+
     char *file_data = strchr(file_list, '|');
+
+
     while (file_data != NULL) {
-        file_data++; 
+
+        file_data++;
+
 
         char *file_info_end = strchr(file_data, '|');
+
         if (file_info_end != NULL) {
             *file_info_end = '\0'; 
 
@@ -129,7 +145,9 @@ void tarsau_a(char *archive_file, char *output_directory) {
             char *file_perm = strtok(NULL, ",");
             char *file_content = strtok(NULL, "|");
 
+
             if (file_name != NULL && file_perm != NULL && file_content != NULL) {
+
                 char output_path[256];
                 snprintf(output_path, sizeof(output_path), "%s/%s", output_directory, file_name);
 
@@ -138,11 +156,16 @@ void tarsau_a(char *archive_file, char *output_directory) {
                     printf("Error creating file: %s\n", file_name);
                     return;
                 }
+
+
                 int size = atoi(file_content);
 
                 FILE *in_fp1 = fopen(archive_file, "r");
+
                 char file_list1[200];
                 fread(file_list1, 1, sizeof(file_list1), in_fp1); 
+                
+
                 int i=toplam;
                 if(i == 0){
                     while(file_list1[i] != '\n'){
@@ -151,6 +174,7 @@ void tarsau_a(char *archive_file, char *output_directory) {
                     i++;
                     toplam=i;
                 }
+               
 
                 for(;i<size+toplam;i++){
                     fprintf(out_fp,"%c",file_list1[i]);
@@ -160,16 +184,21 @@ void tarsau_a(char *archive_file, char *output_directory) {
 
                 *file_info_end = '|'; 
                 file_data = strchr(file_info_end + 1, '|'); 
+    
                 strcpy(created_files[total_files],file_name);
                 total_files++;
+                
                 
             } 
             else {
                 printf("Invalid file format: %s\n", file_data);
+                return;
             }
-        } else {
+            
+        } 
+        else {
             printf("Invalid file format: '|' separator not found.\n");
-            break;
+            return;
         }
     }
 
@@ -178,6 +207,7 @@ void tarsau_a(char *archive_file, char *output_directory) {
     }
 
     printf("Files opened in the directory: %s\n", output_directory);
+
 }
 
 int main(int argc, char *argv[]) {
@@ -185,6 +215,7 @@ int main(int argc, char *argv[]) {
         printf("You must use tarsau -b input files or tarsau -a input files\n");
         return 0;
     }
+
 
 
     if(argc<3){
@@ -197,18 +228,21 @@ int main(int argc, char *argv[]) {
     char *archive_file = argv[2];
     char *output_directory = argv[3];
 
+
     for(int i=2;i<argc;i++){
-    if(strcmp(argv[i],"-o") == 0){
-        if(argv[i+1] != NULL){
-            outputf=argv[i+1];
-            i++;
+        if(strcmp(argv[i],"-o") == 0){
+            if(argv[i+1] != NULL){
+                outputf=argv[i+1];
+                i++;
+            }
+            else{
+                printf("error: missing filename after ‘-o’\n");
+                return 0;
+            }
         }
-    else{
-        printf("error: missing filename after ‘-o’\n");
-        return 0;
     }
-    }
-    }
+
+
     if(strcmp(argv[1],"-b")==0){
         if(argc >= 32){
         printf("ERROR Tarsau -b : The number of input files cannot be more than 32\n");
@@ -216,6 +250,8 @@ int main(int argc, char *argv[]) {
         }
         tarsau_b(argc,argv,outputf);
     }
+
+
     else if(strcmp(argv[1],"-a")==0){
         if(argc >4){
             printf("ERROR Tarsau -a : The number of input files cannot be more than 4\n");
@@ -223,6 +259,7 @@ int main(int argc, char *argv[]) {
         }
         tarsau_a(archive_file,output_directory);
     }
+    
 
     return 1;
 }
